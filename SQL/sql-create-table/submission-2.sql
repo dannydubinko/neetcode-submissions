e6249d62eb5862e764154e0fb71f178eae3c7fc7
Daniel Dubinko <104874364+dannydubinko@users.noreply.github.com>
2026-09-15 18:39:56 -0400

    Add: sql-create-table - submission-2

---
+++ SQL/sql-create-table/submission-2.sql
@@ -0,0 +1,18 @@
+create table videos (
+id int,
+name text,
+createD_at date,
+published BOOLEAN 
+
+);
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
