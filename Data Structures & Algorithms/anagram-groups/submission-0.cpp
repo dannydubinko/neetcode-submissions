7ec76b3943b1723a30168fa934479bffa816c34a
Daniel Dubinko <104874364+dannydubinko@users.noreply.github.com>
2026-09-15 16:25:26 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, int> pairs;
+        vector<vector<string>> output;
+
+        string temp;
+        int counter = 0, index = 0;
+        
+
+        for(string str : strs){
+            temp = str;
+            sort(temp.begin(), temp.end());
+            if(!pairs.contains(temp)){
+                pairs[temp] = counter;
+                vector<string> temp_string = {str};
+                output.push_back(temp_string);
+                counter++;
+                
+            } else {
+                index = pairs[temp];
+                output[index].push_back(str);
+            }
+        }
+        return output;
+    }
+};
+ 
