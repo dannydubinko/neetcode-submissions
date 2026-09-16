a83801be527d66ded39615ac63246aa25698a20f
Daniel Dubinko <104874364+dannydubinko@users.noreply.github.com>
2026-09-16 16:01:02 -0400

    Add: sql-insert-default-values - submission-0

---
+++ SQL/sql-insert-default-values/submission-0.sql
@@ -0,0 +1,21 @@
+CREATE TABLE products (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  stock INTEGER DEFAULT 0
+);
+
+-- Do not modify above this line --
+
+insert into products 
+values (1, 'Apple'),
+(2,'Banana'),
+(3, 'Orange');
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT * FROM products;
